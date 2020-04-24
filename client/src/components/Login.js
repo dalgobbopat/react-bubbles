c7a4a53dd16b3100819779dea786722ad82fe770
Patrick Dal Gobbo <juandalgobbo@Juans-Air.lan1>
2020-04-24 12:12:44 -0400

    created login form, changedusername and password

--- client/src/components/Login.js
+++ client/src/components/Login.js
@@ -1,13 +1,42 @@
-import React from "react";
-import React, {useState} from 'react';
+import React, {useState} from "react";
 import axios from 'axios';
 
-const Login = props =>{
+
+const Login = props => {
+  const [credentials, setCredentials] = useState({
+      username: "",
+      password: ""
+  });
+
+  const onChangeHandler = e => {
+      return setCredentials({
+          ...credentials,
+          [e.target.name]: e.target.value
+      });
+  };
+
+  const handleSubmit = e => {
+      e.preventDefault();
+      axios
+          .post("http://localhost:5000/api/login", credentials)
+          .then(res => {
+              localStorage.setItem("token", res.data.payload);
+              props.history.push("bubbles");
+          })
+          .catch(err => console.log(err));
+  };
   return (
-    <>
-      <h1>Welcome to the Bubble App!</h1>
-      <p>Build a login page here</p>
-    </>
+      <div>
+          <form onSubmit={handleSubmit}>
+              <input type="text" name="username" onChange={onChangeHandler} />
+              <input
+                  type="password"
+                  name="password"
+                  onChange={onChangeHandler}
+              />
+              <button>Log in</button>
+          </form>
+      </div>
   );
 };
 
